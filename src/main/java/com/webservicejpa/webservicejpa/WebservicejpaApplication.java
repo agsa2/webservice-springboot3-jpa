a5d842862052c2ba46c722076fccab23e70d3d9b
agsa2 <agsa0108@gmail.com>
2023-11-07 19:40:50 -0300

    Preject created

---
+++ src/main/java/com/webservicejpa/webservicejpa/WebservicejpaApplication.java
@@ -0,0 +1,13 @@
+package com.webservicejpa.webservicejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicejpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicejpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webservicejpa/webservicejpa/WebservicejpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservicejpa.webservicejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicejpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
