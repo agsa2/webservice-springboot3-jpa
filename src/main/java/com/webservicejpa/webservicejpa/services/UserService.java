a86afd37b52bfad65adfcc882e04e2d30991ed9c
agsa2 <agsa0108@gmail.com>
2023-12-30 11:30:22 -0300

    Exception handling - findById

--- src/main/java/com/webservicejpa/webservicejpa/services/UserService.java
+++ src/main/java/com/webservicejpa/webservicejpa/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.webservicejpa.webservicejpa.entities.User;
 import com.webservicejpa.webservicejpa.repositories.UserRepository;
+import com.webservicejpa.webservicejpa.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
